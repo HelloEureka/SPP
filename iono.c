db4c459c616f2d3bafbf38d3fcab21fa182f166f
Junqiang Lee <1479218182@qq.com>
2020-10-15 16:12:25 +0800

    origin trop and iono

---
+++ iono.c
@@ -0,0 +1,63 @@
+#include "global.h"
+#include <math.h>
+/* ionosphere model ------------------------------------------------------------
+* compute ionospheric delay by broadcast ionosphere model (klobuchar model)
+* args   : gtime_t t        I   time (gpst)
+*          double *ion      I   iono model parameters {a0,a1,a2,a3,b0,b1,b2,b3}
+*          double *pos      I   receiver position {lat,lon,h} (rad,m)
+*          double *azel     I   azimuth/elevation angle {az,el} (rad)
+* return : ionospheric delay (L1) (m)
+*-----------------------------------------------------------------------------*/
+
+/*euclid norm*/
+double norm(const double *a,int n)
+{
+    double res=0;
+    for (int i = 0; i < n; i++) {
+        res+=a[i]*a[i];
+    }
+    return sqrt(res);
+}
+
+
+extern double ionmodel(gtime_t t, const double *ion, const double *pos,
+                       const double *azel)
+{
+    const double ion_default[]={ /* 2004/1/1 */
+        0.1118E-07,-0.7451E-08,-0.5961E-07, 0.1192E-06,
+        0.1167E+06,-0.2294E+06,-0.1311E+06, 0.1049E+07
+    };
+    double tt,f,psi,phi,lam,amp,per,x;
+    int week;
+    
+    if (pos[2]<-1E3||azel[1]<=0) return 0.0;
+    if (norm(ion,8)<=0.0) ion=ion_default;
+    
+    /* earth centered angle (semi-circle) */
+    psi=0.0137/(azel[1]/PI+0.11)-0.022;
+    
+    /* subionospheric latitude/longitude (semi-circle) */
+    phi=pos[0]/PI+psi*cos(azel[0]);
+    if      (phi> 0.416) phi= 0.416;
+    else if (phi<-0.416) phi=-0.416;
+    lam=pos[1]/PI+psi*sin(azel[0])/cos(phi*PI);
+    
+    /* geomagnetic latitude (semi-circle) */
+    phi+=0.064*cos((lam-1.617)*PI);
+
+    /* local time (s) */
+    tt=43200.0*lam + time2gpst(t, &week);
+    tt-=floor(tt/86400.0)*86400.0; /* 0<=tt<86400 */
+    
+    /* slant factor */
+    f=1.0+16.0*pow(0.53-azel[1]/PI,3.0);
+    
+    /* ionospheric delay */
+    amp=ion[0]+phi*(ion[1]+phi*(ion[2]+phi*ion[3]));
+    per=ion[4]+phi*(ion[5]+phi*(ion[6]+phi*ion[7]));
+    amp=amp<    0.0?    0.0:amp;
+    per=per<72000.0?72000.0:per;
+    x=2.0*PI*(tt-50400.0)/per;
+    
+    return CLIGHT*f*(fabs(x)<1.57?5E-9+amp*(1.0+x*x*(-0.5+x*x/24.0)):5E-9);
+}
---
+++ time.c
@@ -0,0 +1,107 @@
+#include "global.h"
+#include <math.h>
+/* convert calendar day/time to time -------------------------------------------
+* convert calendar day/time to gtime_t struct
+* args   : double *ep       I   day/time {year,month,day,hour,min,sec}
+* return : gtime_t struct
+* notes  : proper in 1970-2037 or 1970-2099 (64bit time_t)
+*-----------------------------------------------------------------------------*/
+extern gtime_t epoch2time(const double *ep)
+{
+    const int doy[]={1,32,60,91,121,152,182,213,244,274,305,335};
+    gtime_t time={0};
+    int days,sec,year=(int)ep[0],mon=(int)ep[1],day=(int)ep[2];
+    
+    if (year<1970||2099<year||mon<1||12<mon) return time;
+    
+    /* leap year if year%4==0 in 1901-2099 */
+    days=(year-1970)*365+(year-1969)/4+doy[mon-1]+day-2+(year%4==0&&mon>=3?1:0);
+    sec=(int)floor(ep[5]);
+    time.time=(time_t)days*86400+(int)ep[3]*3600+(int)ep[4]*60+sec;
+    time.sec=ep[5]-sec;
+    return time;
+}
+/* time to calendar day/time ---------------------------------------------------
+* convert gtime_t struct to calendar day/time
+* args   : gtime_t t        I   gtime_t struct
+*          double *ep       O   day/time {year,month,day,hour,min,sec}
+* return : none
+* notes  : proper in 1970-2037 or 1970-2099 (64bit time_t)
+*-----------------------------------------------------------------------------*/
+extern void time2epoch(gtime_t t, double *ep)
+{
+    const int mday[]={ /* # of days in a month */
+        31,28,31,30,31,30,31,31,30,31,30,31,31,28,31,30,31,30,31,31,30,31,30,31,
+        31,29,31,30,31,30,31,31,30,31,30,31,31,28,31,30,31,30,31,31,30,31,30,31
+    };
+    int days,sec,mon,day;
+    
+    /* leap year if year%4==0 in 1901-2099 */
+    days=(int)(t.time/86400);
+    sec=(int)(t.time-(time_t)days*86400);
+    for (day=days%1461,mon=0;mon<48;mon++) {
+        if (day>=mday[mon]) day-=mday[mon]; else break;
+    }
+    ep[0]=1970+days/1461*4+mon/12; ep[1]=mon%12+1; ep[2]=day+1;
+    ep[3]=sec/3600; ep[4]=sec%3600/60; ep[5]=sec%60+t.sec;
+}
+/* gps time to time ------------------------------------------------------------
+* convert week and tow in gps time to gtime_t struct
+* args   : int    week      I   week number in gps time
+*          double sec       I   time of week in gps time (s)
+* return : gtime_t struct
+*-----------------------------------------------------------------------------*/
+extern gtime_t gpst2time(int week, double sec)
+{
+    gtime_t t=epoch2time(gpst0);
+    
+    if (sec<-1E9||1E9<sec) sec=0.0;
+    t.time+=(time_t)86400*7*week+(int)sec;
+    t.sec=sec-(int)sec;
+    return t;
+}
+/* time to gps time ------------------------------------------------------------
+* convert gtime_t struct to week and tow in gps time
+* args   : gtime_t t        I   gtime_t struct
+*          int    *week     IO  week number in gps time (NULL: no output)
+* return : time of week in gps time (s)
+*-----------------------------------------------------------------------------*/
+extern double time2gpst(gtime_t t, int *week)
+{
+    gtime_t t0=epoch2time(gpst0);
+    time_t sec=t.time-t0.time;
+    int w=(int)(sec/(86400*7));
+    
+    if (week) *week=w;
+    return (double)(sec-(double)w*86400*7)+t.sec;
+}
+/* galileo system time to time -------------------------------------------------
+* convert week and tow in galileo system time (gst) to gtime_t struct
+* args   : int    week      I   week number in gst
+*          double sec       I   time of week in gst (s)
+* return : gtime_t struct
+*-----------------------------------------------------------------------------*/
+extern gtime_t gst2time(int week, double sec)
+{
+    gtime_t t=epoch2time(gst0);
+    
+    if (sec<-1E9||1E9<sec) sec=0.0;
+    t.time+=(time_t)86400*7*week+(int)sec;
+    t.sec=sec-(int)sec;
+    return t;
+}
+/* time to galileo system time -------------------------------------------------
+* convert gtime_t struct to week and tow in galileo system time (gst)
+* args   : gtime_t t        I   gtime_t struct
+*          int    *week     IO  week number in gst (NULL: no output)
+* return : time of week in gst (s)
+*-----------------------------------------------------------------------------*/
+extern double time2gst(gtime_t t, int *week)
+{
+    gtime_t t0=epoch2time(gst0);
+    time_t sec=t.time-t0.time;
+    int w=(int)(sec/(86400*7));
+    
+    if (week) *week=w;
+    return (double)(sec-(double)w*86400*7)+t.sec;
+}
